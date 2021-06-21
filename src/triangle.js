4b245c1b4b9ce79371da4eb6b290ef7f8f09f8a8
Kris Hopper <hopperdavis@gmail.com>
2021-06-20 20:55:13 -0700

    add and update files for project

--- src/triangle.js
+++ src/triangle.js
@@ -1,4 +1,4 @@
-function Triangle (side1, side2, side3) {
+export default function Triangle (side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
@@ -7,8 +7,6 @@ function Triangle (side1, side2, side3) {
 Triangle.prototype.checkType = function() {
   return "I can't answer that yet!";
 }
-
-
 
 $(document).ready(function()  {
   $('#triangle-checker-form').submit(function(event)  {
